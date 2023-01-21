4fada23dab8cbea69fac5a48c9a8f0d352a3af5a
aladdinalizada <tu7c5zneb@code.edu.az>
2023-01-22 01:24:27 +0400

    added hero section and featured section

--- client/src/pages/courses-price/index.jsx
+++ client/src/pages/courses-price/index.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Slide } from "react-slideshow-image";
+import { Link } from "react-router-dom";
 
 const FeaturedCourses = () => {
-  return (
-    <div>FeaturedCourses</div>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default FeaturedCourses
+  const getData = async () => {
+    const response = await fetch("http://localhost:3000/");
+    const data = await response.json();
+    setData(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+  return (
+    <div>
+      <div className="box">
+        <div className="box-header">
+          <h2 className="box-title"> Our featured courses</h2>
+        </div>
+        <div className="box-body">
+          {data.slice(0, 3).map((item) => (
+            <div className="card">
+              <div className="image">
+                <img src={item.image} alt="image" />
+              </div>
+              <div className="content-bos">
+                <div className="category">
+                  <span>{item.category}</span>
+                </div>
+                <div className="title">
+                  <h3>{item.title}</h3>
+                </div>
+                <div className="description">
+                  <p>{item.description}</p>
+                </div>
+                <div className="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star-half-stroke"></i>
+                </div>
+                <div className="bottom">
+                  <div className="left">
+                    <span>(4.5)</span> based on 120
+                  </div>
+                  <div className="price">
+                    <span>${item.price}</span>
+                  </div>
+                </div>
+                <button>
+                  <Link>Find Out More</Link>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedCourses;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,83 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let featured = [
+  {
+    id: 1,
+    category: "User Experience",
+    title: "Fundamental of UX for Application design",
+    description:
+      "The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.",
+    rating: "based on 120",
+    price: "135",
+    image:
+      "https://preview.colorlib.com/theme/courses/assets/img/gallery/featured1.png.webp",
+  },
+  {
+    id: 2,
+    category: "User Experience",
+    title: "Fundamental of UX for Application design",
+    description:
+      "The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.",
+    rating: "based on 120",
+    price: "135",
+    image:
+      "https://preview.colorlib.com/theme/courses/assets/img/gallery/featured2.png.webp",
+  },
+  {
+    id: 3,
+    category: "User Experience",
+    title: "Fundamental of UX for Application design",
+    description:
+      "The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.",
+    rating: "based on 120",
+    price: "135",
+    image:
+      "https://preview.colorlib.com/theme/courses/assets/img/gallery/featured3.png.webp",
+  },
+  {
+    id: 4,
+    category: "User Experience",
+    title: "Fundamental of UX for Application design",
+    description:
+      "The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.",
+    rating: "based on 120",
+    price: "135",
+    image:
+      "https://preview.colorlib.com/theme/courses/assets/img/gallery/featured1.png.webp",
+  },
+  {
+    id: 5,
+    category: "User Experience",
+    title: "Fundamental of UX for Application design",
+    description:
+      "The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.",
+    rating: "based on 120",
+    price: "135",
+    image:
+      "https://preview.colorlib.com/theme/courses/assets/img/gallery/featured2.png.webp",
+  },
+  {
+    id: 6,
+    category: "User Experience",
+    title: "Fundamental of UX for Application design",
+    description:
+      "The automated process all your website tasks. Discover tools and techniques to engage effectively with vulnerable children and young people.",
+    rating: "based on 120",
+    price: "135",
+    image:
+      "https://preview.colorlib.com/theme/courses/assets/img/gallery/featured3.png.webp",
+  },
+];
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send(featured));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
