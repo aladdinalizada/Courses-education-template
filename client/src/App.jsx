d1f2e742adc148d17e02df78c72588b5468c1a7d
aladdinalizada <tu7c5zneb@code.edu.az>
2023-01-22 05:38:25 +0400

    scroll to top completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import BackToTop from "./components/back-to-top";
 import Footer from "./layouts/footer";
 import Header from "./layouts/header";
 import Hero from "./pages/introduction";
@@ -8,10 +7,33 @@ import Routing from "./routing";
 function App() {
   return (
     <div className="App">
-      <BackToTop/>
       <Header />
       <Routing />
       <Footer />
+      <div style={{ height: "1rem" }} />
+      <button
+        onClick={() => {
+          window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        }}
+        style={{
+          position: "fixed",
+          padding: "1.3rem",
+          fontSize: "20px",
+          bottom: "40px",
+          right: "40px",
+          backgroundColor: "#c86fff",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlignLast: "center",
+          color: "#fff",
+          textAlign: "center",
+        }}
+      >
+        <i class="fa-sharp fa-solid fa-turn-up" style={{
+          padding: "0.8rem",
+        }}></i>
+      </button>
     </div>
   );
 }
